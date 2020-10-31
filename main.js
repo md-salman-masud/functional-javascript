008488ff4e900bea79c1da161b9778bbdffc6855
md.salman masud <mdsalmanraselmasud@gmail.com>
2020-10-31 14:45:22 +0600

    createa map fn

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 var arr=[1,2,3,4,5,6,7,8,9,14,545,566,45,7,85,];
 
 function stucture(arr,callback){
-    var newarr=[];
+    var newarr =[];
     for(var i=0; i<arr.length; i++){
-       newarr.push(callback=arr[i]);
+       newarr.push(callback(arr[i]));
        
     }
 return newarr;
 }
 var map=stucture(arr,function(call){
-    return call +(5*5);
+    return call +(5+5);
     
 
 })
