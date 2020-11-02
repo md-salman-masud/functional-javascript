fc89e246214aa8762a76bd345d23255ccd62b39d
md.salman masud <mdsalmanraselmasud@gmail.com>
2020-11-02 12:46:34 +0600

    added  a file

---
+++ practice/obj-orient-js/prototype.js
@@ -0,0 +1,30 @@
+// let obj={
+//     name:'rasel hosain',
+//     age:21,
+//     email:'mdsalmanmasud@gmail.com'
+
+// }
+
+
+
+// constructor
+
+function Person(name,age,email){
+this.name=name;
+this.age=age;
+this.email=email;
+}
+
+var p1=new Person('md.salmanMasud',22,'mdsalmanmasud@gamil.com','abc');
+var p2=new Person('md.raselHosain',21,'mdusmangoni926@gamil.com','abc');
+var p3=new Person('mss.aysha akter',40,'ayshaakter@gamil.com','abc');
+console.log(p1);
+
+Person.prototype.protpholio='  +'web development';
+Person.prototype.hello=function(){
+ console.log('hello : '+this.name+this.protpholio);
+}
+p1.hello();
+p2.hello();
+p3.hello();
+console.log(p1);
